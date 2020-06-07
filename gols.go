3bc438e2208d6406fd6f783d3916c71af748aa69
Raiu1210 <baio1484@gmail.com>
2020-06-07 16:46:55 +0900

    -a command added

--- gols.go
+++ gols.go
@@ -5,21 +5,59 @@ import (
     "io/ioutil"
     "log"
     "os"
+    "strings"
 )
 
-func main() {
+
+func handleInput(args []string) {
+    if len(args) > 1 {
+        if args[1] == "-a" {
+            load_all_file_list()
+        }
+    } else {
+        load_file_list()
+    }
+}
+
+
+func load_file_list() {
     // get current directory
     dir, err := os.Getwd()
     if err != nil {
         log.Fatal(err)
     }
 
-    fileInfos, err := ioutil.ReadDir(dir)
+    fileInfoList, err := ioutil.ReadDir(dir)
     if err != nil {
         log.Fatal(err)
     }
 
-    for _, fileInfo := range fileInfos {
+    for _, fileInfo := range fileInfoList {
+        if strings.HasPrefix(fileInfo.Name(), ".") {
+            continue
+        }
+
         fmt.Println(fileInfo.Name())
     }
+}
+
+
+func load_all_file_list() {
+    dir, err := os.Getwd()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fileInfoList, err := ioutil.ReadDir(dir)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, fileInfo := range fileInfoList {
+        fmt.Println(fileInfo.Name())
+    }
+}
+
+func main() {
+    handleInput(os.Args)
 }
